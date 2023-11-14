919f5eb93fae90e94a7aa2fa371bd11bc8c906f2
bakloutifares <fares.baklouti@esprit.tn>
2023-11-14 16:39:01 +0100

    last not least

--- src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
+++ src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
@@ -1,7 +1,10 @@
 package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,6 +14,10 @@ public class GestionStationSkiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionStationSkiApplication.class, args);
+	}
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
