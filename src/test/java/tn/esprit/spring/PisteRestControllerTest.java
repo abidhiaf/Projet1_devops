da6a4d7b323066d691caa02ab0d4a09028f0ccb0
Ghadaaoun <ghada.aoun@esprit.tn>
2023-11-15 22:19:09 +0100

    Update PisteRestControllerTest.java

--- src/test/java/tn/esprit/spring/PisteRestControllerTest.java
+++ src/test/java/tn/esprit/spring/PisteRestControllerTest.java
@@ -1,5 +1,10 @@
-package tn.esprit.spring;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,63 +24,63 @@ public class PisteRestControllerTest {
     public void testAddPiste() {
         // Créez un objet Piste factice pour les tests
         Piste piste = new Piste();
-        piste.setNom("Ma piste de test");
+        piste.setNamePiste("Ma piste de test"); // Correction du nom du champ
 
         // Appelez la méthode du contrôleur que vous voulez tester
         Piste result = pisteRestController.addPiste(piste);
 
         // Effectuez des assertions pour vérifier que le résultat est conforme aux attentes
         assertNotNull(result);
-        assertEquals("Ma piste de test", result.getNom());
+        assertEquals("Ma piste de test", result.getNamePiste());
     }
 
     @Test
     public void testGetAllPistes() {
         // Créez une liste factice de pistes pour les tests
-        Piste piste1 = new Piste("Piste 1");
-        Piste piste2 = new Piste("Piste 2");
+        Piste piste1 = new Piste();
+        piste1.setNamePiste("Piste 1");
+        Piste piste2 = new Piste();
+        piste2.setNamePiste("Piste 2");
         List<Piste> pistes = new ArrayList<>();
         pistes.add(piste1);
         pistes.add(piste2);
-    
+
         // Appelez la méthode du contrôleur que vous voulez tester
         List<Piste> result = pisteRestController.getAllPistes();
-    
+
         // Effectuez des assertions pour vérifier que le résultat est conforme aux attentes
         assertNotNull(result);
-        assertEquals(2, result.size()); // Vérifiez le nombre d'éléments dans la liste, par exemple
-        assertEquals("Piste 1", result.get(0).getNom());
-        assertEquals("Piste 2", result.get(1).getNom());
+        assertEquals(2, result.size());
+        assertEquals("Piste 1", result.get(0).getNamePiste());
+        assertEquals("Piste 2", result.get(1).getNamePiste());
     }
-    
+
     @Test
     public void testGetById() {
         // Créez un objet Piste factice pour les tests
         Piste piste = new Piste();
-        piste.setNom("Ma piste de test");
+        piste.setNamePiste("Ma piste de test");
         Long pisteId = 1L;
-    
+
         // Appelez la méthode du contrôleur que vous voulez tester
         Piste result = pisteRestController.getById(pisteId);
-    
+
         // Effectuez des assertions pour vérifier que le résultat est conforme aux attentes
         assertNotNull(result);
-        assertEquals(pisteId, result.getId());
-        assertEquals("Ma piste de test", result.getNom());
+        assertEquals(pisteId, result.getNumPiste()); // Correction du nom du champ
+        assertEquals("Ma piste de test", result.getNamePiste());
     }
-    
+
     @Test
     public void testDeleteById() {
         // Identifiant de la piste à supprimer
         Long pisteId = 1L;
-    
+
         // Appelez la méthode du contrôleur que vous voulez tester
         pisteRestController.deleteById(pisteId);
-    
+
         // Vous pouvez ajouter des assertions supplémentaires pour vérifier la suppression, par exemple
         Piste deletedPiste = pisteRestController.getById(pisteId);
         assertNull(deletedPiste);
     }
-    
-    
 }
